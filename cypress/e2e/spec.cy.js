d9ca83dc01716fc2be4813a4f262fd6b50de373c
PreciousAmine <goodnesstchaya810@gmail.com>
2025-03-13 08:50:46 +0100

    Implementation of Attribute and XPath Selectors

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,38 +1,50 @@
 /// <reference types="cypress" />
-describe('template spec', () => {
+describe('Cypress test', () => {
   it('passes', () => {
     cy.visit('/');
-    cy.get("#user-name").type("visual_user");
-    cy.get("#password").type("secret_sauce");
-    cy.get("#login-button").click();
-    // I assert thet the logo should be visible
-    cy.get(".app_logo").should("be.visible");
-    // I changed the order sorting from high to low 
-    cy.get(".product_sort_container").select("hilo");
-    //I added items to the cart
-    cy.get(".inventory_item").eq(0).find(".btn_inventory").click();
-    cy.get(".inventory_item").eq(1).find(".btn_inventory").click();
-    cy.get(".inventory_item").eq(2).find(".btn_inventory").click();
-    cy.get(".inventory_item").eq(5).find(".btn_inventory").click();
-    cy.get(".inventory_item").eq(4).find(".btn_inventory").click();
-    //I proceeded to the cart
-    cy.get(".shopping_cart_link").click();
-    // I proceeded to checkout
-    cy.get("#checkout").click();
-    // I entered checkout information
-    cy.get("#first-name").type("Precious");
-    cy.get("#last-name").type("Amine");
-    cy.get("#postal-code").type("Makepe")
-    cy.get("#continue").click();
-    // I finished the checkout 
-    cy.get("#finish").click();
+    //Using attribute selectors
+     // Assert that we are on the login page
+    cy.url().should("include", "saucedemo.com"); //Update to assert that URL is equal to https://www.saucedemo.com/
+    cy.get("input[placeholder='Username']").type("visual_user");
+    cy.get("input[placeholder='Password']").type("secret_sauce");
+    cy.get("input[type='submit']").click();
+    cy.wait(2000);
+    // Assert that the logo should be visible
+    cy.get('div.app_logo').should('be.visible');
+    cy.wait(1000);
+    // Change the order sorting from high to low 
+    cy.get('[data-test="product-sort-container"]').select('hilo'); 
+    cy.wait(2000);
+    // Add items to the cart
+    cy.get('button[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+    cy.get('button[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('button[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    cy.wait(2000);
+    // Remove one item from the cart
+    cy.get('button[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
+    cy.wait(2000);
+    // Proceed to checkout
+    cy.get('a.shopping_cart_link').click();
+    cy.get('button[data-test="checkout"]').click();
+    cy.wait(2000);
+    // Enter checkout information
+    cy.get('input[data-test="firstName"]').type('Johnathan');
+    cy.get('input[data-test="lastName"]').type('Donald');
+    cy.get('input[data-test="postalCode"]').type('12544');
+    cy.get('input[data-test="continue"]').click();
+    cy.wait(2000);
+    // Finish the checkout 
+    cy.get('button[data-test="finish"]').click();
+    cy.wait(2000);
+    //Using XPath
     // Assert that the complete order message is visible
-    cy.get(".complete-header").should("contain","Thank you for your order!" );
+    cy.xpath('//h2[contains(text(), "Thank you for your order!")]').should('be.visible');
+    cy.wait(2000);
     // Logout
-    cy.get("#react-burger-menu-btn").click();
-    cy.get("#logout_sidebar_link").click(); 
-     // Assert that we are back on the login page
-    cy.url().should("include", "saucedemo.com");
+    cy.xpath('//button[@id="react-burger-menu-btn"]').click();
+    cy.wait(1000);
+    cy.xpath('//a[@id="logout_sidebar_link"]').click();
+    cy.wait(2000);
 
   })
 })
